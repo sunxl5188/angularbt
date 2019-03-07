76416e5e57bb3c19d15af2f7ad62e55d2b199711
364864264 <364864264@qq.com>
2019-03-07 17:35:35 +0800

    提交

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormGroup, FormBuilder, Validators, FormControl} from '@angular/forms';
-import {Hero} from '../hero';
+import {FormGroup, FormBuilder, Validators, FormControl, FormArray} from '@angular/forms';
 
 @Component({
     selector: 'app-form',
@@ -8,44 +7,129 @@ import {Hero} from '../hero';
     styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
-    model = new Hero('', '', '', '', '');
-    private heroForm: FormGroup;
-
-    mobileValidator(control: FormControl) {
-
-        const mobileReg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
-        const result = mobileReg.test(control.value);
-        return result ? null : {mobile: {info: '请输入正确的手机号'}};
-    }
-
-    equalValidator(group: FormGroup): any {
-
-        const password = group.get('password') as FormControl;
-        const pconfirm = group.get('pconfirm') as FormControl;
-
-        const isEqule: boolean = (password.value === pconfirm.value);
-        return isEqule ? null : {equal: {info: '两次密码不一致'}};
-    }
-
     constructor(private fb: FormBuilder) {
-        this.heroForm = this.fb.group({
-            username: ['', [Validators.required, Validators.minLength(6)]],
-            mobile: ['', this.mobileValidator],
-            passwordGroup: this.fb.group({
-                password: ['', Validators.minLength(6)],
-                pconfirm: ['']
-            }, {validator: this.equalValidator})
-        });
+
     }
 
-    onSubmit() {
-        console.log(this.model.username);
+    myform: FormGroup;
+
+    formErrors = {
+        username: ''
     }
+
+    validationMessage = {
+        username: {
+            minlength: '用户名长度最少为4个字符',
+            maxlength: '用户名长度最多为10个字符',
+            required: '请填写用户名',
+            notdown: '用户名不能以下划线开头',
+            only: '用户名只能包含数字、字母、下划线'
+        }
+    };
+
+    /*myform = new FormGroup({
+        username: new FormControl(''),
+        email: new FormControl(''),
+        password: new FormControl(''),
+        password1: new FormControl(''),
+        birthday: new FormControl(''),
+        gender: new FormControl(''),
+        languages: new FormGroup({
+            lang1: new FormControl(''),
+            lang2: new FormControl(''),
+            lang3: new FormControl(''),
+            lang4: new FormControl(''),
+            lang5: new FormControl('')
+        }),
+        address: new FormGroup({
+            prov: new FormControl(''),
+            city: new FormControl(''),
+            dist: new FormControl(''),
+            street: new FormControl('')
+        })
+    })*/
+
+    buildForm() {
+        this.myform = this.fb.group({
+            username: ['', [
+                Validators.required,
+                Validators.minLength(4),
+                Validators.maxLength(10)
+            ]],
+            email: [''],
+            password: [''],
+            password1: [''],
+            birthday: [''],
+            gender: [''],
+            languages: this.fb.group({
+                lang1: [''],
+                lang2: [''],
+                lang3: [''],
+                lang4: [''],
+                lang5: ['']
+            }),
+            address: this.fb.group({
+                prov: [''],
+                city: [''],
+                dist: [''],
+                street: ['']
+            }),
+            aliases: this.fb.array([
+                this.fb.control('')
+            ])
+        })
+        // 每次表单数据发生变化的时候更新错误信息
+        this.myform.valueChanges
+            .subscribe(data => this.onValueChanged(data));
+
+        // 初始化错误信息
+        this.onValueChanged();
+
+    }
+
+    onValueChanged(data ?: any) {
+        // 如果表单不存在则返回
+        if (!this.myform) return;
+        // 获取当前的表单
+        const form = this.myform;
+        // 遍历错误消息对象
+        for (const field in this.formErrors) {
+            // 清空当前的错误消息
+            this.formErrors[field] = '';
+            // 获取当前表单的控件
+            const control = form.get(field);
+
+            // 当前表单存在此空间控件 && 此控件没有被修改 && 此控件验证不通过
+            if (control && control.dirty && !control.valid) {
+                // 获取验证不通过的控件名，为了获取更详细的不通过信息
+                const messages = this.validationMessage[field];
+                // 遍历当前控件的错误对象，获取到验证不通过的属性
+                for (const key in control.errors) {
+                    // 把所有验证不通过项的说明文字拼接成错误消息
+                    this.formErrors[field] += messages[key] + '\n';
+                }
+            }
+        }
+    }
+
 
     ngOnInit() {
         laydate.render({
             elem: '#time'
-        })
+        });
+        this.buildForm();
+    }
+
+    onSubmit() {
+        console.warn(this.myform.value);
+    }
+
+    get aliases() {
+        return this.myform.get('aliases') as FormArray;
+    }
+
+    addAlias() {
+        this.aliases.push(this.fb.control(''));
     }
 
 }
