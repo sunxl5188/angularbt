00af58e5ec2b0d27b1944f70c333553d7ed67858
364864264 <364864264sun>
2019-03-14 21:57:06 +0800

    提交

--- src/app/detail/detail.service.ts
+++ src/app/detail/detail.service.ts
@@ -8,13 +8,11 @@ export interface Config {
     content: string;
 }
 
-const httpOptions = {
-    headers: new HttpHeaders({
-        // 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-        'Content-Type': 'application/json;charset=UTF-8',
-        'X-CustomToken': 'value++++++'
-    })
-};
+const headers = new HttpHeaders({
+    // 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+    'Content-Type': 'application/json;charset=UTF-8',
+    'X-CustomToken': 'value+'
+})
 
 @Injectable()
 
@@ -32,7 +30,8 @@ export class ConfigService {
     getConfig() {
         const params = new HttpParams()
             .set('id', '123');
-        return this.http.get<Config>(this.configUrl, {params})
+
+        return this.http.get<Config>(this.configUrl, { headers, params })
             .pipe(
                 retry(3), // 最多重试3次失败的请求
                 catchError(this.handleError) // 然后处理错误
@@ -40,8 +39,10 @@ export class ConfigService {
     }
 
     getConfigResponse(): Observable<HttpResponse<Config>> {
+        const params = new HttpParams()
+        .set('id', '1212');
         return this.http.get<Config>(
-            this.configUrl, {observe: 'response'}
+            this.configUrl, {observe: 'response', headers, params}
         );
     }
 
