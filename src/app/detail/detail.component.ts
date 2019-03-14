62116f2a44c209b35dff61b2a38e0683cc399f6f
364864264 <364864264@qq.com>
2019-03-14 18:10:10 +0800

    提交

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -38,4 +38,16 @@ export class DetailComponent implements OnInit {
             );
     }
 
+    showConfigResponse() {
+        this.configService.getConfigResponse()
+            .subscribe(
+                resp => {
+                    const keys = resp.headers.keys();
+                    this.headers = keys.map(key => `${key}: ${resp.headers.get(key)}`);
+                    this.config = {...resp.body};
+                },
+                error => this.error = error
+            );
+    }
+
 }
--- src/app/detail/detail.service.ts
+++ src/app/detail/detail.service.ts
@@ -1,18 +1,28 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {HttpErrorResponse, HttpResponse} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse, HttpResponse, HttpHeaders, HttpParams} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
 import {catchError, retry} from 'rxjs/operators';
 
 export interface Config {
-    heroesUrl: string;
-    textfile: string;
+    title: string;
+    content: string;
 }
+
+const httpOptions = {
+    headers: new HttpHeaders({
+        // 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        'Content-Type': 'application/json;charset=UTF-8',
+        'X-CustomToken': 'value++++++'
+    })
+};
 
 @Injectable()
 
 export class ConfigService {
-    configUrl = 'assets/config.json';
+    // configUrl = 'assets/config.json';
+    configUrl = 'http://www.js.me/demo/data.php?action=all';
+
+    // configUrl = 'http://www.applyoversea.com/Mobile/School/mList';
 
     constructor(
         private  http: HttpClient
@@ -20,26 +30,34 @@ export class ConfigService {
     }
 
     getConfig() {
-        return this.http.get<Config>(this.configUrl)
+        const params = new HttpParams()
+            .set('id', '123');
+        return this.http.get<Config>(this.configUrl, {params})
             .pipe(
                 retry(3), // 最多重试3次失败的请求
                 catchError(this.handleError) // 然后处理错误
             );
     }
 
+    getConfigResponse(): Observable<HttpResponse<Config>> {
+        return this.http.get<Config>(
+            this.configUrl, {observe: 'response'}
+        );
+    }
+
     private handleError(error: HttpErrorResponse) {
         if (error.error instanceof ErrorEvent) {
-            // A client-side or network error occurred. Handle it accordingly.
-            console.error('An error occurred:', error.error.message);
+            // 发生客户端或网络错误。相应处理。
+            console.error('发生了一个错误:', error.error.message);
         } else {
-            // The backend returned an unsuccessful response code.
-            // The response body may contain clues as to what went wrong,
+            // 后端返回一个不成功的响应代码。
+            // 响应主体可能包含关于出错原因的线索，
             console.error(
-                `Backend returned code ${error.status}, ` +
-                `body was: ${error.error}`);
+                `后端返回代码 ${error.status}, ` +
+                `错误内容: ${error.error}`);
         }
         // return an observable with a user-facing error message
         return throwError(
-            'Something bad happened; please try again later.');
+            '访问出现问题，请稍后再试!');
     }
 }
