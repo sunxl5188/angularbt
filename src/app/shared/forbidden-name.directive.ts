516dab2a63469fac9c41d3c5a7077f7c44e1cd72
364864264 <364864264@qq.com>
2019-08-20 17:57:41 +0800

    提交

--- src/app/shared/forbidden-name.directive.ts
+++ src/app/shared/forbidden-name.directive.ts
@@ -39,3 +39,23 @@ export function emailValidator(): ValidatorFn {
         return isEmail ? null : {email: {error: '请输入正确的邮箱地址!'}};
     };
 }
+
+// 正则表达式验证
+export function validateValidator(nameType): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+        let isTrue;
+        let reg;
+        switch (nameType) {
+            case 'alpha':
+                reg = /^[A-Z]*$/i;
+                break;
+        }
+        isTrue = reg.test(control.value);
+        return isTrue ? null : {};
+    };
+}
+
+// 验证字符只能包含字母
+// 验证字符只能包含字母,数字，短划线或下划线
+// 验证字段可能包含字母字符或数字
+// 验证字段可能包含字母字符或空格
